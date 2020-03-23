1630dc6b4815c791f1c40ebd37ec8bf66c5a4c68
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-22 19:03:01 -0700

    demonstrate how blocking operations can hold up single-threaded Node

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,17 @@
 function start() {
   console.log("Request handler 'start' was clled.");
+
+  function sleep(milliSeconds) {
+    const startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
   return 'Hello Start';
 }
 
 function upload() {
-  console.log("Request handler 'upload' was clled.");
+  console.log("Request handler 'upload' was called.");
   return 'Hello Upload';
 }
 
