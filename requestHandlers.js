a8313020ba8f58fbd871d41c18e7962de6bafb70
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-22 18:46:25 -0700

    Request handlers return content to browser (ie, how not to set up request response)

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
-  console.log("Request handler 'start' was clled.")
+  console.log("Request handler 'start' was clled.");
+  return 'Hello Start';
 }
 
 function upload() {
-  console.log("Request handler 'upload' was clled.")
+  console.log("Request handler 'upload' was clled.");
+  return 'Hello Upload';
 }
 
 exports.start = start;
