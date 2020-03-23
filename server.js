bc79770f8c02875de286924c173182ec4c4a7329
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-22 19:26:17 -0700

    use callbacks to respond to requests with non-blocking operations

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const http = require('http');
 const url = require("url");
 
-// distinguish requests based on URL path (paths for /start and /uploard URLs are handled by different parts of code)
 function start(route, handle) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + "received.");
 
-    const content = route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
